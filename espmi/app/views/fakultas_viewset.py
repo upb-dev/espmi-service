db06becc99d5283eb8a6a5cacb9c480f364e7954
Sarikhin <sarikhinlovestory@gmail.com>
2023-06-27 06:12:46 +0700

    [update] add fakultas search

--- espmi/app/views/fakultas_viewset.py
+++ espmi/app/views/fakultas_viewset.py
@@ -7,3 +7,4 @@ from espmi.app.views.base_model_viewset import BaseModelViewSet
 class FakultasViewSet(BaseModelViewSet):
     queryset = Fakultas.objects.all().order_by("created_at")
     serializer_class = FakultasSerializer
+    search_fields = ['name']
